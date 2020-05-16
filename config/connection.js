5aa75debbbe6f58393919c296e111a345dbc0401
samdotkim <60358344+samdotkim@users.noreply.github.com>
2020-05-15 23:16:12 -0500

    commit before pull

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "somethingdumb",
+  password: "password",
   database: "stockwatch_db"
 });
 
